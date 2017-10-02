b04bd324c0110aef7d6f22aeec1c33d03f1a5229
David Sanchez <jdsancheze@outlook.com>
2017-10-02 16:59:53 -0500

    fix lint error

--- test/GithubApi.Redirect.js
+++ test/GithubApi.Redirect.js
@@ -5,13 +5,16 @@ const chai = require('chai');
 const { expect } = chai;
 
 describe('Given a HEAD tests', () => {
-  it('Then the agent must receive a redirect with HEAD Method', () => agent.head('https://github.com/aperdomob/redirect-test')
-    .auth('token', process.env.ACCESS_TOKEN).catch((response) => {
-      expect(response.status).to.equal(statusCode.MOVED_PERMANENTLY);
-      expect(response.response.res.headers.location).to.equal('https://github.com/aperdomob/new-redirect-test');
-    }));
-  it('Then the agent must receive a redirect with GET Method', () => agent.get('https://github.com/aperdomob/redirect-test')
-    .auth('token', process.env.ACCESS_TOKEN).then((response) => {
-      expect(response.status).to.equal(statusCode.OK);
-    }));
+  it('Then the agent must receive a redirect with HEAD Method', () =>
+    agent.head('https://github.com/aperdomob/redirect-test')
+      .auth('token', process.env.ACCESS_TOKEN).catch((response) => {
+        expect(response.status).to.equal(statusCode.MOVED_PERMANENTLY);
+        expect(response.response.res.headers.location).to
+          .equal('https://github.com/aperdomob/new-redirect-test');
+      }));
+  it('Then the agent must receive a redirect with GET Method', () =>
+    agent.get('https://github.com/aperdomob/redirect-test')
+      .auth('token', process.env.ACCESS_TOKEN).then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+      }));
 });
